2f3a3c1acb3ba93ad94ad12f0e008f25f0fdd4d0
Hidran Arias <hidran.arias@spryker.com>
2022-10-28 19:25:04 +0200

    Adds user's detail page and link in users page

--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -1,3 +1,4 @@
+import { UserDetailComponent } from './../user-detail/user-detail.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -7,6 +8,10 @@ const routes: Routes = [
   {
     path: '',
     component: UsersPage
+  },
+  {
+    path: ':id/detail',
+    component: UserDetailComponent
   }
 ];
 
