335f1adcee7bed0e9d477029305cfa879bc2b85d
Hidran Arias <hidran.arias@spryker.com>
2022-10-28 19:31:28 +0200

    Adds back button to user detail page

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,14 +1,23 @@
+
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-detail',
-  templateUrl: './user-detail.component.html',
-  styleUrls: ['./user-detail.component.scss'],
+  templateUrl: './user-detail.component.html'
 })
 export class UserDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {
 
-  ngOnInit() {}
+  }
+
+  ngOnInit() { }
+  goBack(evt) {
+    //this.router.navigate(['users'])
+    this.router.navigateByUrl('users');
+  }
+
+
 
 }
