bff8ae8760e6c96bc3af1512d3e85115a03d8f75
Hidran Arias <hidran.arias@spryker.com>
2022-10-29 18:14:22 +0200

    Riproduci 30. Leggere i dati dalla cache sia per elenco utenti che per dettaglio

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,7 +1,6 @@
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {map, tap} from 'rxjs/operators';
-import {Observable} from 'rxjs';
 import {User, Users} from '../interfaces/user-interfaces';
 import {Storage} from '@ionic/storage-angular';
 
@@ -13,19 +12,39 @@ export class UsersService {
   apiURL = 'https://randomuser.me/api/?seed=usersapp&results=100&inc=email,gender,name,nat,picture,dob,phone,login&noinfo';
 
   constructor(private http: HttpClient, private storage: Storage) {
-    this.storage.create();
+
   }
 
-  getUsers(): Observable<User[]> {
+  async getUsers() {
+    await this.storage.create();
+    const users = await this.storage.get('users');
+
+    if (users) {
+      return users;
+    }
+
     return this.http.get<Users>(this.apiURL).pipe(
       map(res => res.results),
-      tap(users => this.storage.set('users', users))
-    );
+      tap(resUsers => {
+
+        this.storage.set('users', resUsers);
+      })
+    ).toPromise();
   }
 
   async getUserById(id: string): Promise<User> {
-    const users = await this.getUsers().toPromise();
-    return users.filter(user => user.login.uuid === id)[0];
+
+    const usercached = await this.storage.get('user-' + id);
+    if (usercached) {
+      return usercached;
+    }
+    const users = await this.getUsers();
+    const userData = users.filter(user => user.login.uuid === id)[0];
+    if (userData) {
+      await this.storage.set('user-' + id, userData);
+    }
+    return userData || [];
   }
+
 }
 
--- src/app/users/users.page.ts
+++ src/app/users/users.page.ts
@@ -2,20 +2,18 @@ import {Component, OnInit} from '@angular/core';
 import {User} from '../interfaces/user-interfaces';
 import {UsersService} from '../services/users.service';
 
-import {Observable} from 'rxjs';
-
 @Component({
   selector: 'app-users',
   templateUrl: './users.page.html',
   styleUrls: ['./users.page.scss'],
 })
 export class UsersPage implements OnInit {
-  public users$: Observable<User[]>;
+  public users$: Promise<User[]>;
 
   constructor(private usersService: UsersService) {
   }
 
-  ngOnInit() {
+  async ngOnInit() {
     this.users$ = this.usersService.getUsers();
   }
 
